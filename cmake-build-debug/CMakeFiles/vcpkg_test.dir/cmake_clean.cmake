6626b6cdead7f6aa6899b4cc28b2772a5395da9c
William Howell <wlm.howell@gmail.com>
2020-03-18 18:22:34 -0400

    init

---
+++ cmake-build-debug/CMakeFiles/vcpkg_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vcpkg_test.dir/main.cpp.o"
+  "vcpkg_test"
+  "vcpkg_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vcpkg_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
